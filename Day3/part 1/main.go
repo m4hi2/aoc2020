eb28eef110ebc945e7289a427b57147ea9cd947c
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2020-12-04 01:34:22 +0600

    solved day 3 part 1 (very crude sol)
    have to figure out how to get line nubmers from Scan method

---
+++ Day3/part 1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	pos := 0
+	tree := 0
+	file, _ := os.Open("./input_file.txt")
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		pos += 3
+		if string(line[pos%len(line)]) == "#" {
+			tree++
+		}
+		fmt.Println(line, pos, pos%len(line))
+		fmt.Println(string(line[pos%len(line)]))
+	}
+	fmt.Println(tree)
+}
